9c51674d783fb36e493015401aad33a1f78cc16f
Cyrus Sethna <csethna@gmail.com>
2017-11-20 14:35:19 -0600

    hard code the requests URL
    Requests library does not handle spaces in the same way the CTA Bus Tracker API wants using `+` instead of `%20`. Requests is having a hard time with escaped characters `\` in Python so the hardcoding of the URL was done to correct for this issue.

--- cta_bus_call.py
+++ cta_bus_call.py
@@ -1,12 +1,10 @@
 import requests
 import urllib
 
-BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops'
+BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops?key=nfLj3RLeuQimpC6zdQwE3b8qj&rt=22&dir=South%20Bound'
 
-params = {'key': 'nfLj3RLeuQimpC6zdQwE3b8qj', 'rt': 22, 'dir': "South Bound"}
-params = urllib.parse.urlencode(params)
-r_south = requests.get(BUS_LOOKUP, params)
+r_south = requests.get(BUS_LOOKUP)
 
-# print(r_south.text)
+print(r_south.text)
 
-print(r_south.url)
+#print(r_south.url)
