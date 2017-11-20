e8db985a293c217f0eaf6f70ee08bb03f7857073
Cyrus Sethna <csethna@gmail.com>
2017-11-20 16:00:21 -0600

    implement credentials

--- cta_bus_call.py
+++ cta_bus_call.py
@@ -1,14 +1,18 @@
 import requests
 import urllib3
 import json
+from credentials import *
 
 #BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getpredictions'
 BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops'
 
 #r_south = requests.get(BUS_LOOKUP, params={'key' : 'nfLj3RLeuQimpC6zdQwE3b8qj', 'stpid' : '1813', 'format' : 'json'})
-r_south = requests.get(BUS_LOOKUP, params={'key' : 'nfLj3RLeuQimpC6zdQwE3b8qj', 'rt' : '22', 'dir' : 'Southbound', 'format' : 'json'})
+r_south = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'rt' : '22', 'dir' : 'Southbound', 'format' : 'json'})
+r_north = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'rt' : '22', 'dir' : 'Northbound', 'format' : 'json'})
 #print(r_south.text)
 #print(r_south.url)
+#print(r_north.text)
+#print(r_north.url)
 
 # Automatically geolocate the connecting IP
 http = urllib3.PoolManager()
--- cta_station_call.py
+++ cta_station_call.py
@@ -8,6 +8,7 @@ r_south = requests.get(STATION_URL, params={'key': 'nfLj3RLeuQimpC6zdQwE3b8qj', 
 r_north = requests.get(STATION_URL, params={'key': 'nfLj3RLeuQimpC6zdQwE3b8qj', 'mapid': '40540', 'stpid': '30105', 'max': '2'})
 
 print(r_south.text)
+print(json.dumps(r_south))
 
 # print('Red line to 95/ Dan Ryan:' + r_south)
 # print('Red line to Howard:' + r_north)
