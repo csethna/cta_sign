efd0d1d3487081987cc99bd4750735317aeef88b
Cyrus Sethna <csethna@gmail.com>
2017-11-20 14:56:19 -0600

    figured out how to get bus stop IDs despite really bad documentation

--- cta_bus_call.py
+++ cta_bus_call.py
@@ -1,10 +1,11 @@
 import requests
 import urllib
 
-BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops?key=nfLj3RLeuQimpC6zdQwE3b8qj&rt=22&dir=South%20Bound'
+#BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getpredictions'
+BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops'
 
-r_south = requests.get(BUS_LOOKUP)
-
+#r_south = requests.get(BUS_LOOKUP, params={'key' : 'nfLj3RLeuQimpC6zdQwE3b8qj', 'stpid' : '1813', 'format' : 'json'})
+r_south = requests.get(BUS_LOOKUP, params={'key' : 'nfLj3RLeuQimpC6zdQwE3b8qj', 'rt' : '22', 'dir' : 'Southbound', 'format' : 'json'})
 print(r_south.text)
 
 #print(r_south.url)
