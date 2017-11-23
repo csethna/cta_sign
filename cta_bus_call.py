91edb7bc8eb26e1ec0e227dd5a1006f8fc34eb7f
Cyrus Sethna <csethna@gmail.com>
2017-11-23 11:15:46 -0500

    added location finder

--- cta_bus_call.py
+++ cta_bus_call.py
@@ -1,27 +1,43 @@
 import requests
-import urllib3
 import json
 from credentials import *
 
 #BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getpredictions'
 BUS_LOOKUP = 'http://www.ctabustracker.com/bustime/api/v2/getstops'
 
-#r_south = requests.get(BUS_LOOKUP, params={'key' : 'nfLj3RLeuQimpC6zdQwE3b8qj', 'stpid' : '1813', 'format' : 'json'})
+## Return next busses for the stop by my house.
+south_22 = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'stpid' : '1813', 'format' : 'json'})
+			# {
+			# 	"stpid": "1813",
+			# 	"stpnm": "Clark \u0026 Sunnyside",
+			# 	"lat": 41.96355882694,
+			# 	"lon": -87.666392326355
+			# },
+north_22 = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'stpid' : '1935', 'format' : 'json'})
+			# {
+			# 	"stpid": "1935",
+			# 	"stpnm": "Clark \u0026 Sunnyside",
+			# 	"lat": 41.963351407875,
+			# 	"lon": -87.666274309158
+			# },
+## Printing
+#print(south_22.text)
+#print(south_22.url)
+#print(north_22.text)
+#print(north_22.url)
+
+## Return list of all stops on the 22 line
 r_south = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'rt' : '22', 'dir' : 'Southbound', 'format' : 'json'})
 r_north = requests.get(BUS_LOOKUP, params={'key' : CTA_BUS, 'rt' : '22', 'dir' : 'Northbound', 'format' : 'json'})
+
+## Printing
 #print(r_south.text)
 #print(r_south.url)
 #print(r_north.text)
 #print(r_north.url)
 
 # Automatically geolocate the connecting IP
-http = urllib3.PoolManager()
 IP_url = 'http://freegeoip.net/json/'
-response = http.request('GET', IP_url)
-response = str(response)
-#location = json.loads(response)
-print(response)
-# location_city = location['city']
-# location_state = location['region_name']
-# location_country = location['country_name']
-# location_zip = location['zipcode']
+response = requests.get(IP_url)
+response = json.loads(response.text)
+print(",".join((str(response["latitude"]), str(response["longitude"]))))
